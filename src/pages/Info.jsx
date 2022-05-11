dd14c0eb532844221d48af2ebcfd36f15db91d49
Eldar <eldarweb@mail.ru>
2022-05-11 15:04:35 +0300

    fix

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -28,12 +28,12 @@ const Info = () => {
         <Layout crumbs={crumbs} activeMenu='info'>
             <div className='w-full flex flex-col text-[22px] font-semibold'>
                 <div className='grid grid-cols-3 gap-[25px] justify-between w-full'>
-                    <Link to='/info/stocks' className='bg-[#F5F5F5] h-[427px] relative'>
+                    <Link to='info/stocks' className='bg-[#F5F5F5] h-[427px] relative'>
                         <img src={stockSvg} alt='' className='w-[303px] h-[295px] m-auto absolute right-0 left-0 top-[-40px] bottom-0' />
                         <span className='absolute bottom-[20px] block w-full text-center'>Акции</span>
                     </Link>
 
-                    <Link to='/info/bonus_conditions' className='bg-[#F5F5F5] h-[427px] relative'>
+                    <Link to='info/bonus_conditions' className='bg-[#F5F5F5] h-[427px] relative'>
                         <img src={usloviyaSvg} alt='' className='w-[303px] h-[295px] m-auto absolute right-0 left-0 top-[-40px] bottom-0' />
                         <span className='absolute bottom-[20px] block w-full text-center'>Условия программы “Бонусы Киргу”</span>
                     </Link>
@@ -43,17 +43,17 @@ const Info = () => {
                         <span className='absolute bottom-[20px] block w-full text-center'>Узнать бонусные баллы</span>
                     </Link>
 
-                    <Link to='/info/delivery' className='bg-[#F5F5F5] h-[427px] relative'>
+                    <Link to='info/delivery' className='bg-[#F5F5F5] h-[427px] relative'>
                         <img src={deliverySvg} alt='' className='w-[303px] h-[295px] m-auto absolute right-0 left-0 top-[-40px] bottom-0' />
                         <span className='absolute bottom-[20px] block w-full text-center'>Доставка</span>
                     </Link>
 
-                    <Link to='/info/services' className='bg-[#F5F5F5] h-[427px] relative'>
+                    <Link to='info/services' className='bg-[#F5F5F5] h-[427px] relative'>
                         <img src={sborkaSvg} alt='' className='w-[248px] h-[275px] m-auto absolute right-0 left-0 top-[-40px] bottom-0' />
                         <span className='absolute bottom-[20px] block w-full text-center'>Услуги</span>
                     </Link>
 
-                    <Link to='/info/contacts' className='bg-[#F5F5F5] h-[427px] relative'>
+                    <Link to='info/contacts' className='bg-[#F5F5F5] h-[427px] relative'>
                         <img src={contactsSvg} alt='' className='w-[319px] h-[297px] m-auto absolute right-0 left-0 top-[-40px] bottom-0' />
                         <span className='absolute bottom-[20px] block w-full text-center'>Наши магазины</span>
                     </Link>
