072231b88f511298a5f77347a5a0cc420007dfc1
Mkhab1b <MKHAB1B@yandex.ru>
2022-08-15 10:04:36 +0300

    фикс столбцов в категориях

--- src/components/CatalogList.jsx
+++ src/components/CatalogList.jsx
@@ -1,7 +1,7 @@
 import React, {  }  from 'react';
 import CatalogItem from "./CatalogItem";
 
-const CatalogList = ({catalog, catalogId, fromAllCats, columns=4}) => {
+const CatalogList = ({catalog, catalogId, fromAllCats, columns='4'}) => {
 
     return(
         <div className={`catalog_content grid grid-cols-${columns} w-full gap-[25px] mt-[15px]`}>
