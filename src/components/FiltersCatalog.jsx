cd64d1db6fd4f61e9c1d4b86f81d8eb67e50687c
Gadzhimurad <g1mbatov@Mail.ru>
2022-08-16 15:38:53 +0300

    Рассрочка

--- src/components/FiltersCatalog.jsx
+++ src/components/FiltersCatalog.jsx
@@ -1,14 +1,14 @@
-import React, {  }  from 'react';
+import React, { } from 'react';
 import PriceFilter from "./PriceRange/PriceFilter";
 import PropFilters from "./PropFilters";
 
-const FiltersCatalog = ({height, filterData, setFilterProp, setPriceFilter}) => {
+const FiltersCatalog = ({ height, filterData, setFilterProp, setPriceFilter }) => {
 
     return (
         <div className='w-[256px] min-h-[765px]'>
             {filterData.price && parseInt(filterData.price.min) > 1 &&
-                <PriceFilter onChange={setPriceFilter} minPrice={filterData.price.min} maxPrice={filterData.price.max} /> 
-            } 
+                <PriceFilter onChange={setPriceFilter} minPrice={filterData.price.min} maxPrice={filterData.price.max} />
+            }
             <PropFilters propsData={filterData.props} setFilterProp={setFilterProp} />
         </div>
     )
--- src/components/PropFilterItem.jsx
+++ src/components/PropFilterItem.jsx
@@ -1,22 +1,24 @@
-import React, {  }  from 'react';
+import React, { } from 'react';
 import Icons from "./Icons";
 
-const PropFilterItem = ({pr, openData, openAccord, setFilterProp}) => {
+const PropFilterItem = ({ pr, openData, openAccord, setFilterProp }) => {
 
     const hiddenCl = openData.id === pr.id && openData.open ? '' : 'hidden'
 
     return (
         <div className="border-b border-[#e6e6e6]">
-            <div className="accord_title flex min-h-[65px] justify-between items-center cursor-pointer" onClick={() => openAccord(pr) }>
+            <div className="accord_title flex min-h-[65px] justify-between items-center cursor-pointer" onClick={() => openAccord(pr)}>
                 <span className="font-bold">{pr.name}</span>
                 <Icons name='pointer_b' className='w-[15px] h-[7px] fill-[#f0a83c]' />
+            
+
             </div>
 
             <div className={`${hiddenCl} pt-[10px]`}>
-                {pr.props.map((prChild, inx) => 
+                {pr.props.map((prChild, inx) =>
                     <label className="flex items-center mb-[20px]" key={inx}>
                         <input type="checkbox" onChange={(e) => setFilterProp(e.target.checked, pr.id, prChild.id)} />
-                        <span className="text-[14px] ml-[12px]">{prChild.name}</span>
+                        <span className="text-[14px] ml-[12px]">{prChild.name.replace(/&quot;/g, '')}</span>
                     </label>
                 )}
             </div>
