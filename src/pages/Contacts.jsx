7f032c0900cf21256e60c56bfc9445a6ba051d9c
Gadzhimurad <g1mbatov@Mail.ru>
2022-08-17 15:27:43 +0300

    fixContacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,31 @@
-import React, {  }  from 'react';
+import React, { } from 'react';
 import Icons from "../components/Icons";
 import Layout from "../layout";
+import { YMaps, Map, Placemark } from "react-yandex-maps";
 
 const Contacts = () => {
 
     const crumbs = [['Информация', 'info'], ['Контакты', 'contacts']]
 
-    return(
+    const mapData = {
+        center: [42.973100, 47.514745],
+        zoom: 8,
+      };
+      
+      const coordinates = [
+        [42.134964, 48.296098],
+        [42.897263, 47.626900],
+        [42.952425, 47.562232],
+        [42.983803, 47.466384],
+        [42.983803, 47.466384],
+        [42.965884, 47.396725],
+        [42.822397, 47.125819],
+        [43.228285, 46.614351],
+        [43.247704, 46.576243],
+        [43.842551, 46.725512]
+      ];
+
+    return (
         <Layout crumbs={crumbs} activeMenu='info'>
             <div className='w-full'>
                 <h2 className='text-[24px] font-semibold'>Наши магазины</h2>
@@ -21,11 +40,11 @@ const Contacts = () => {
                             <Icons className='w-[24px] h-[24px]' name='mail' />
                             <span>info@kirgu.ru</span>
                         </div>
-                        
+
                     </div>
 
                     <div className='w-[620px] pr-[25px]'>
-                        
+
                         <div className='pt-[10px] pb-[10px] border-b border-[#e6e6e6]'>
                             <div className='flex space-x-[10px] mb-[7px]'>
                                 <Icons className='w-[25px] h-[27px]' name='map' />
@@ -43,7 +62,7 @@ const Contacts = () => {
                                 <span>Домашний, г. Каспийск, ул.Ленина 60а</span>
                             </div>
                             <div className='flex space-x-[10px] text-[#505050]'>
-                            <Icons className='w-[24px] h-[24px]' name='clock' />
+                                <Icons className='w-[24px] h-[24px]' name='clock' />
                                 <span>10:00-20:00</span>
                             </div>
                         </div>
@@ -139,9 +158,12 @@ const Contacts = () => {
                     </div>
 
                     <div className='w-[850px] pl-[25px]'>
-                        <script type="text/javascript" charSet="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A6d4254cdd59f4d6225c43d7ae9fdb5de52df5979b3f17127bcff41a37d31f74d&amp;width=850&amp;height=809&amp;lang=ru_RU&amp;scroll=true" />
+                        <YMaps>
+                            <Map defaultState={mapData} width='100%' height='100%'>
+                                {coordinates.map((coordinate, index) => <Placemark geometry={coordinate} key={index} />)}
+                            </Map>
+                        </YMaps>
                     </div>
-
                 </div>
             </div>
         </Layout>
